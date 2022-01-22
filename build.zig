6f82e49011e7a4bf1eaa3d656a5c1ae501e7663a
Igor Stojkovic <stojkovic.igor+git@gmail.com>
2022-01-22 14:05:29 +0100

    Added zmath external library

--- build.zig
+++ build.zig
@@ -7,6 +7,7 @@ pub fn build(b: *std.build.Builder) void {
 
     const main_tests = b.addTest("src/main.zig");
     main_tests.setBuildMode(mode);
+    main_tests.addPackagePath("zmath", "libs/zmath.zig");
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
