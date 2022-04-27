073c04ca76d5b0801d5cff10a722f0ab2f7852a9
Igor Stojkovic <stojkovic.igor+git@gmail.com>
2022-04-27 12:33:24 +0200

    Simplified FileReader so it has fixed buffer size

--- src/io.zig
+++ src/io.zig
@@ -6,78 +6,75 @@ const assert = std.debug.assert;
 
 pub const ImageReadError = error{ EndOfStream, SeekError } || std.os.ReadError;
 
-pub fn ImageReader(comptime buffer_size_type: type) type {
-    return union(enum) {
-        const FReader = FileReader(buffer_size_type);
+pub const ImageReader = union(enum) {
+    buffer: BufferReader,
+    file: FileReader,
 
-        buffer: BufferReader,
-        file: FReader,
+    const Self = @This();
 
-        const Self = @This();
+    pub fn fromFile(file: std.fs.File) Self {
+        return Self{ .file = FileReader.init(file) };
+    }
 
-        pub fn fromFile(file: std.fs.File) Self {
-            return Self{ .file = FReader.init(file) };
+    pub fn fromMemory(buffer: []const u8) Self {
+        return Self{ .buffer = BufferReader.init(buffer) };
+    }
+
+    pub fn wrap(fileOrBuffer: anytype) Self {
+        if (@TypeOf(fileOrBuffer) == FileReader) return .{ .file = fileOrBuffer };
+        if (@TypeOf(fileOrBuffer) == BufferReader) return .{ .buffer = fileOrBuffer };
+        @compileError("ImageReader can only wrap FileReader and BufferReader");
+    }
+
+    pub inline fn readNoAlloc(self: *Self, size: usize) ImageReadError![]const u8 {
+        switch (self.*) {
+            .buffer => |*b| return b.readNoAlloc(size),
+            .file => |*f| return f.readNoAlloc(size),
         }
+    }
 
-        pub fn fromMemory(buffer: []const u8) Self {
-            return Self{ .buffer = BufferReader.init(buffer) };
+    pub inline fn read(self: *Self, buf: []u8) ImageReadError!usize {
+        switch (self.*) {
+            .buffer => |*b| return b.read(buf),
+            .file => |*f| return f.read(buf),
         }
+    }
 
-        pub inline fn readNoAlloc(self: *Self, size: buffer_size_type) ImageReadError![]const u8 {
-            switch (self.*) {
-                .buffer => |*b| return b.readNoAlloc(size),
-                .file => |*f| return f.readNoAlloc(size),
-            }
+    pub inline fn readStruct(self: *Self, comptime T: type) ImageReadError!*const T {
+        switch (self.*) {
+            .buffer => |*b| return b.readStruct(T),
+            .file => |*f| return f.readStruct(T),
         }
+    }
 
-        pub inline fn read(self: *Self, buf: []u8) ImageReadError!usize {
-            switch (self.*) {
-                .buffer => |*b| return b.read(buf),
-                .file => |*f| return f.read(buf),
-            }
+    pub inline fn readInt(self: *Self, comptime T: type) ImageReadError!T {
+        switch (self.*) {
+            .buffer => |*b| return b.readInt(T),
+            .file => |*f| return f.readInt(T),
         }
+    }
 
-        pub inline fn readStruct(self: *Self, comptime T: type) ImageReadError!*const T {
-            switch (self.*) {
-                .buffer => |*b| return b.readStruct(T),
-                .file => |*f| return f.readStruct(T),
-            }
+    pub fn readIntBig(self: *Self, comptime T: type) ImageReadError!T {
+        switch (self.*) {
+            .buffer => |*b| return b.readIntBig(T),
+            .file => |*f| return f.readIntBig(T),
         }
+    }
 
-        pub inline fn readInt(self: *Self, comptime T: type) ImageReadError!T {
-            switch (self.*) {
-                .buffer => |*b| return b.readInt(T),
-                .file => |*f| return f.readInt(T),
-            }
+    pub fn readIntLittle(self: *Self, comptime T: type) ImageReadError!T {
+        switch (self.*) {
+            .buffer => |*b| return b.readIntLittle(T),
+            .file => |*f| return f.readIntLittle(T),
         }
+    }
 
-        pub fn readIntBig(self: *Self, comptime T: type) ImageReadError!T {
-            switch (self.*) {
-                .buffer => |*b| return b.readIntBig(T),
-                .file => |*f| return f.readIntBig(T),
-            }
+    pub fn seekBy(self: *Self, amt: i64) ImageReadError!void {
+        switch (self.*) {
+            .buffer => |*b| return b.seekBy(amt),
+            .file => |*f| return f.seekBy(amt),
         }
-
-        pub fn readIntLittle(self: *Self, comptime T: type) ImageReadError!T {
-            switch (self.*) {
-                .buffer => |*b| return b.readIntLittle(T),
-                .file => |*f| return f.readIntLittle(T),
-            }
-        }
-
-        pub fn seekBy(self: *Self, amt: i64) ImageReadError!void {
-            switch (self.*) {
-                .buffer => |*b| return b.seekBy(amt),
-                .file => |*f| return f.seekBy(amt),
-            }
-        }
-    };
-}
-
-pub const ImageReader8 = ImageReader(u8);
-pub const ImageReader10 = ImageReader(u10);
-pub const ImageReader12 = ImageReader(u12);
-pub const ImageReader16 = ImageReader(u16);
+    }
+};
 
 pub const BufferReader = struct {
     buffer: []const u8,
@@ -162,110 +159,99 @@ pub const BufferReader = struct {
     }
 };
 
-pub fn FileReader(comptime buffer_size_type: type) type {
-    const typeInfo = @typeInfo(buffer_size_type);
-    if (typeInfo != .Int or typeInfo.Int.signedness != .unsigned or typeInfo.Int.bits < 8 or typeInfo.Int.bits > 18) {
-        @compileError("The buffer size type must be unsigned int of at least 8 and at most 18 bits!");
+pub const FileReader = struct {
+    file: std.fs.File,
+    pos: usize = 0,
+    end: usize = 0,
+    buffer: [16 * 1024]u8 = undefined,
+
+    const Self = @This();
+
+    pub fn init(file: std.fs.File) Self {
+        return Self{ .file = file };
     }
-    const capacity = std.math.maxInt(buffer_size_type) + 1;
-    return struct {
-        file: std.fs.File,
-        pos: usize = 0,
-        end: usize = 0,
-        buffer: [capacity]u8 = undefined,
 
-        const Self = @This();
-
-        pub fn init(file: std.fs.File) Self {
-            return Self{ .file = file };
-        }
-
-        pub fn readNoAlloc(self: *Self, size: buffer_size_type) ImageReadError![]const u8 {
-            var available = self.end - self.pos;
-            if (available < size) {
-                mem.copy(u8, self.buffer[0..available], self.buffer[self.pos..self.end]);
-                var readSize = try self.file.read(self.buffer[available..]);
-                self.pos = 0;
-                available += readSize;
-                self.end = available;
-            }
-            if (available < size) return error.EndOfStream;
-
-            var endPos = self.pos + size;
-            var result = self.buffer[self.pos..endPos];
-            self.pos = endPos;
-            return result;
-        }
-
-        pub fn read(self: *Self, buf: []u8) ImageReadError!usize {
-            var size = buf.len;
-            var available = self.end - self.pos;
-            if (available >= size) {
-                var endPos = self.pos + size;
-                mem.copy(u8, buf[0..], self.buffer[self.pos..endPos]);
-                self.pos = endPos;
-                return size;
-            }
-
-            mem.copy(u8, buf[0..available], self.buffer[self.pos..self.end]);
+    pub fn readNoAlloc(self: *Self, size: usize) ImageReadError![]const u8 {
+        if (size > self.buffer.len) return error.EndOfStream;
+        var available = self.end - self.pos;
+        if (available < size) {
+            mem.copy(u8, self.buffer[0..available], self.buffer[self.pos..self.end]);
+            var readSize = try self.file.read(self.buffer[available..]);
             self.pos = 0;
-            self.end = 0;
-            return self.file.read(buf[available..]);
+            available += readSize;
+            self.end = available;
+        }
+        if (available < size) return error.EndOfStream;
+
+        var endPos = self.pos + size;
+        var result = self.buffer[self.pos..endPos];
+        self.pos = endPos;
+        return result;
+    }
+
+    pub fn read(self: *Self, buf: []u8) ImageReadError!usize {
+        var size = buf.len;
+        var available = self.end - self.pos;
+        if (available >= size) {
+            var endPos = self.pos + size;
+            mem.copy(u8, buf[0..], self.buffer[self.pos..endPos]);
+            self.pos = endPos;
+            return size;
         }
 
-        pub fn readStruct(self: *Self, comptime T: type) ImageReadError!*const T {
-            // Only extern and packed structs have defined in-memory layout.
-            comptime assert(@typeInfo(T).Struct.layout != std.builtin.TypeInfo.ContainerLayout.Auto);
-            const size = @sizeOf(T);
-            if (size > self.buffer.len) return error.EndOfStream;
-            var buf = try self.readNoAlloc(size);
-            return @ptrCast(*const T, buf);
+        mem.copy(u8, buf[0..available], self.buffer[self.pos..self.end]);
+        self.pos = 0;
+        self.end = 0;
+        return self.file.read(buf[available..]);
+    }
+
+    pub fn readStruct(self: *Self, comptime T: type) ImageReadError!*const T {
+        // Only extern and packed structs have defined in-memory layout.
+        comptime assert(@typeInfo(T).Struct.layout != std.builtin.TypeInfo.ContainerLayout.Auto);
+        const size = @sizeOf(T);
+        if (size > self.buffer.len) return error.EndOfStream;
+        var buf = try self.readNoAlloc(size);
+        return @ptrCast(*const T, buf);
+    }
+
+    pub fn readInt(self: *Self, comptime T: type) ImageReadError!T {
+        comptime assert(@typeInfo(T) == .Int);
+        const bitSize = @bitSizeOf(T);
+        const size = @sizeOf(T);
+        comptime assert(bitSize % 8 == 0 and bitSize / 8 == size); // This will not allow u24 as intended
+        var result: T = undefined;
+        var readSize = try self.read(mem.asBytes(&result));
+        if (readSize != size) return error.EndOfStream;
+        return result;
+    }
+
+    pub fn readIntBig(self: *Self, comptime T: type) ImageReadError!T {
+        return mem.bigToNative(T, try self.readInt(T));
+    }
+
+    pub fn readIntLittle(self: *Self, comptime T: type) ImageReadError!T {
+        return mem.littleToNative(T, try self.readInt(T));
+    }
+
+    pub fn seekBy(self: *Self, amt: i64) ImageReadError!void {
+        if (amt < 0) {
+            const absAmt = std.math.absCast(amt);
+            const absAmtUsize = std.math.cast(usize, absAmt) catch std.math.maxInt(usize);
+            self.pos -|= absAmtUsize;
+        } else {
+            const amtUsize = std.math.cast(usize, amt) catch std.math.maxInt(usize);
+            const newPos = self.pos +| amtUsize;
+            if (newPos > self.end) return error.SeekError;
+            self.pos = newPos;
         }
+    }
 
-        pub fn readInt(self: *Self, comptime T: type) ImageReadError!T {
-            comptime assert(@typeInfo(T) == .Int);
-            const bitSize = @bitSizeOf(T);
-            const size = @sizeOf(T);
-            comptime assert(bitSize % 8 == 0 and bitSize / 8 == size); // This will not allow u24 as intended
-            var result: T = undefined;
-            var readSize = try self.read(mem.asBytes(&result));
-            if (readSize != size) return error.EndOfStream;
-            return result;
-        }
+    pub const Reader = std.io.Reader(*Self, ImageReadError, read);
 
-        pub fn readIntBig(self: *Self, comptime T: type) ImageReadError!T {
-            return mem.bigToNative(T, try self.readInt(T));
-        }
-
-        pub fn readIntLittle(self: *Self, comptime T: type) ImageReadError!T {
-            return mem.littleToNative(T, try self.readInt(T));
-        }
-
-        pub fn seekBy(self: *Self, amt: i64) ImageReadError!void {
-            if (amt < 0) {
-                const absAmt = std.math.absCast(amt);
-                const absAmtUsize = std.math.cast(usize, absAmt) catch std.math.maxInt(usize);
-                self.pos -|= absAmtUsize;
-            } else {
-                const amtUsize = std.math.cast(usize, amt) catch std.math.maxInt(usize);
-                const newPos = self.pos +| amtUsize;
-                if (newPos > self.end) return error.SeekError;
-                self.pos = newPos;
-            }
-        }
-
-        pub const Reader = std.io.Reader(*Self, ImageReadError, read);
-
-        pub fn reader(self: *Self) Reader {
-            return .{ .context = self };
-        }
-    };
-}
-
-pub const FileReader8 = FileReader(u8);
-pub const FileReader10 = FileReader(u10);
-pub const FileReader12 = FileReader(u12);
-pub const FileReader16 = FileReader(u16);
+    pub fn reader(self: *Self) Reader {
+        return .{ .context = self };
+    }
+};
 
 // ********************* TESTS *********************
 
@@ -275,17 +261,17 @@ test "FileReader" {
     defer cwd.deleteFile("test.tmp") catch {};
     var file = try cwd.openFile("test.tmp", .{ .mode = .read_only });
     defer file.close();
-    var reader = ImageReader8.fromFile(file);
+    var reader = ImageReader.fromFile(file);
     try testReader(&reader);
 }
 
 test "BufferReader" {
     var buffer = "0123456789Abcdefghijklmnopqr0123456789";
-    var reader = ImageReader8.fromMemory(buffer[0..]);
+    var reader = ImageReader.fromMemory(buffer[0..]);
     try testReader(&reader);
 }
 
-fn testReader(reader: *ImageReader8) !void {
+fn testReader(reader: *ImageReader) !void {
     var array10 = try reader.readNoAlloc(10);
     try std.testing.expectEqualSlices(u8, "0123456789", array10);
     const TestStruct = packed struct {
