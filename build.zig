36045274e5b5d89ec438607529a425d8d1c3db99
Igor Stojkovic <stojkovic.igor+git@gmail.com>
2022-05-07 16:50:17 +0200

    Fixed build so launch can target a test executable

--- build.zig
+++ build.zig
@@ -8,6 +8,7 @@ pub fn build(b: *std.build.Builder) void {
     const main_tests = b.addTest("ziggyimg.zig");
     main_tests.setBuildMode(mode);
     main_tests.addPackagePath("zmath", "libs/zmath.zig");
+    main_tests.emit_bin = .{ .emit_to = "zig-out/ziggyimgtest"};
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
